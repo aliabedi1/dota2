c56888ffa6e81fd9b81d9edaa8a2b85bab6ad991
ali abedi <ali.abedi9731@yahoo.com>
2023-06-04 20:20:10 +0330

    added more properties of torment to the class

--- Game/Map/Objects/Tormentor.php
+++ Game/Map/Objects/Tormentor.php
@@ -6,10 +6,14 @@ class Tormentor
     const magicResist = 55;
     const baseHitPoint = 2500;
     const hpRegen = 100;
+    const firstRespawnTime = 1200;
 
-
-
+    
+    
     // dynamic properties of tormentor in patch 7.33c
+    private $time = 0;
+    private $killedTime;
+    private $nextRespawnTime = $this->killedTime + 600;
     private $spawn = 1;
     private $AdditionalHitPoint = ($this->spawn - 1) * 200 ;
     private $hpNow;
@@ -31,7 +35,6 @@ class Tormentor
 
 
 
-    
 
-    
+
 }
