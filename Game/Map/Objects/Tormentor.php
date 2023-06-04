860c3e7ea5ff8e4edfc3a6e36624a57fa6d53813
ali abedi <ali.abedi9731@yahoo.com>
2023-06-04 21:40:30 +0330

    changed logic for the construct method

--- Game/Map/Objects/Tormentor.php
+++ Game/Map/Objects/Tormentor.php
@@ -13,35 +13,40 @@ class Tormentor
     
     // dynamic properties of tormentor in patch 7.33c
     private $time = 0;
-    private $killedTime;
+    private $killedTime = 0;
     private $nextRespawnTime = $this->killedTime + 600;
-    private $spawn = 0;
+    private $spawn;
     private $AdditionalHitPoint = ($this->spawn - 1) * 200 ;
     private $hpNow;
 
 
     public function __construct($time = 0)
     {
-        if ($time > 0)
+        if($time >= self::firstRespawnTime)
         {
-            if($time >= self::firstRespawnTime)
+            if($this->killedTime && $time > $this->killedTime)
             {
-                if($this->killedTime && $time > $this->killedTime)
-                {
-                    $this->spawn += 1;
-                }
-                $this->spawn = 1;
+                $this->spawn += 1;
             }
-            $this->spawn = 0;
-            $this->notRespawned();
+            $this->spawn = 1;
         }
         else
         {
-            $this->time = 0;
+            $this->spawn = 0;
+            $this->notRespawned();
         }
+
         $this->hpNow = self::baseHitPoint + $this->AdditionalHitPoint;
     }
 
+    private function kill($time)
+    {
+        $this->killedTime = $time;
+    }
+    private function spawn()
+    {
+        $this->spawn += 1;
+    }
 
 
     private function notRespawned()
