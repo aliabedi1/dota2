21f6061b717bfa89121486166716896170529c7b
ali abedi <ali.abedi9731@yahoo.com>
2023-06-04 21:43:30 +0330

    changing logic compeletly

--- Game/Map/Objects/Tormentor.php
+++ Game/Map/Objects/Tormentor.php
@@ -8,51 +8,23 @@ class Tormentor
     const baseHitPoint = 2500;
     const hpRegen = 100;
     const firstRespawnTime = 1200;
+    const respawnTimeDuration = 600;
 
     
     
     // dynamic properties of tormentor in patch 7.33c
-    private $time = 0;
-    private $killedTime = 0;
-    private $nextRespawnTime = $this->killedTime + 600;
-    private $spawn;
-    private $AdditionalHitPoint = ($this->spawn - 1) * 200 ;
-    private $hpNow;
+    private $time;
+    private $spawnedCount;
+    private $diedAt;
+    private $nextRespawnTime;
+
 
 
     public function __construct($time = 0)
     {
-        if($time >= self::firstRespawnTime)
-        {
-            if($this->killedTime && $time > $this->killedTime)
-            {
-                $this->spawn += 1;
-            }
-            $this->spawn = 1;
-        }
-        else
-        {
-            $this->spawn = 0;
-            $this->notRespawned();
-        }
-
-        $this->hpNow = self::baseHitPoint + $this->AdditionalHitPoint;
+       
     }
 
-    private function kill($time)
-    {
-        $this->killedTime = $time;
-    }
-    private function spawn()
-    {
-        $this->spawn += 1;
-    }
-
-
-    private function notRespawned()
-    {
-        return self::name . 'is not spawned';
-    }
 
 
 
