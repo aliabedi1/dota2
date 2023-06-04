0421d145a70a1e194c4360885c38b9790f1207b3
ali abedi <ali.abedi9731@yahoo.com>
2023-06-05 03:23:52 +0330

    change function types and adding functions for spawn and time setters and getters

--- Game/Map/Objects/Tormentor.php
+++ Game/Map/Objects/Tormentor.php
@@ -3,7 +3,7 @@
 class Tormentor
 {
     // static properties of tormentor in patch 7.33c
-    const name = 'Tormentor';
+    const name = 'Tormentor ';
     const magicResist = 55;
     const baseHitPoint = 2500;
     const hpRegen = 100;
@@ -26,24 +26,114 @@ class Tormentor
     }
 
     // manage game time
-    public function startTime()
+    private function startTime()
     {
         $this->time = 0;
     }
-    public function setTime(int $time)
+    private function setTime(int $time)
     {
         $this->time = $time;
     }
 
-    public function getTime()
+    private function getTime()
     {
         return $this->time;
     }
 
-    
+    private function setSpawnedCount(int $spawnedCount)
+    {
+        if($this->time < self::firstRespawnTime)
+        {
+            return self::name . 'cant respawn before 20:00 minute\n';
+        }
+        $this->$this = $spawnedCount;
+    }
+
+    private function getSpawnedCount()
+    {
+        return $this->spawnedCount;
+    }
+
+    private function spawn() : bool
+    {
+        if($this->isSpawnable())
+        {
+            $this->spawnedCount ++;
+            $this->setTimeForSpawns();
+            echo self::name .'spawned';
+            return true;
+        }
+        return false;
+    }
+
+    private function spawnMany($count) : bool
+    {
+        if($this->isSpawnable() && $this->isSpawnableManyTimes($count))
+        {
+            $this->spawnedCount += $count;
+            $this->setTimeForSpawns($count);
+            echo self::name .'spawned ' .$count . ' times';
+            return true;
+        }
+        return false;
+    }
+
+    private function isSpawnable() : bool
+    {
+        if($this->time < self::firstRespawnTime)
+        {
+            echo self::name . 'cant respawn before 20:00 minute\n';
+            return false;
+        }
+        return true;
+    }
+    private function isSpawnableManyTimes($count) : bool
+    {
+        $sForTimes = $count == 1 ? ' time' : ' times';
+        $message = self::name . ' cant spawn '. $count . $sForTimes;
+        $availableTime = $this->getAvailableTime();
+        if($availableTime <= 0)
+        {
+            echo $message;
+            return false;
+        }
+        $availableCount = $availableTime / self::respawnTimeDuration; 
+        return $count == $availableCount;
+    }
+    private function getAvailableTime()
+    {
+        if($this->time < self::firstRespawnTime)
+        {
+            echo self::name . ' didn\'t respawn';
+            return 0;
+        }
+        if($this->diedAt)
+        {
+            return $availableTime = $this->time - $this->diedAt;  
+        }
+        
+        return $availableTime = $this->time - self::firstRespawnTime;
+    }
+
+    private function setTimeForSpawns($count = null)
+    {
+        if($count)
+        {
+            if($this->diedAt)
+            {
+                return $this->setTime($this->diedAt + ($count * self::respawnTimeDuration));
+            }
+            return $this->setTime(self::firstRespawnTime + ($count * self::respawnTimeDuration));
+            
+        }
+        else
+        {
+            return $this->setTime(self::firstRespawnTime);
+        }
+    }
 
 
-
+    // todo: update time at killing or spawning
 
 
 
