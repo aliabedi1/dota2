693c92eb11dbf91c2dd6ad6d7733a888b5701b67
ali abedi <ali.abedi9731@yahoo.com>
2023-06-04 20:45:50 +0330

    managing time conditions for torment class constructor method

--- Game/Map/Objects/Tormentor.php
+++ Game/Map/Objects/Tormentor.php
@@ -3,6 +3,7 @@
 class Tormentor
 {
     // static properties of tormentor in patch 7.33c
+    const name = 'Tormentor';
     const magicResist = 55;
     const baseHitPoint = 2500;
     const hpRegen = 100;
@@ -14,18 +15,38 @@ class Tormentor
     private $time = 0;
     private $killedTime;
     private $nextRespawnTime = $this->killedTime + 600;
-    private $spawn = 1;
+    private $spawn = 0;
     private $AdditionalHitPoint = ($this->spawn - 1) * 200 ;
     private $hpNow;
 
 
-    public function __construct($spawn = null)
+    public function __construct($time = 0)
     {
-        if ($spawn)
+        if ($time > 0)
         {
-            $this->spawn = $spawn;
+            if($time >= self::firstRespawnTime)
+            {
+                if($this->killedTime && $time > $this->killedTime)
+                {
+                    $this->spawn += 1;
+                }
+                $this->spawn = 1;
+            }
+            $this->spawn = 0;
+            $this->notRespawned();
+        }
+        else
+        {
+            $this->time = 0;
         }
         $this->hpNow = self::baseHitPoint + $this->AdditionalHitPoint;
+    }
+
+
+
+    private function notRespawned()
+    {
+        return self::name . 'is not spawned';
     }
 
 
